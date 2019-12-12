d217781c0d18ac843e47e0a634e3a516b2b19a5e
Pat Brown <ptrckbrown94@gmail.com>
2019-12-12 08:55:27 -0600

    added htlm templates and folder, added additional sudo code

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 /*
 build 4 classes to pass tests
-import prompt library from readme
-ask how many employees for the team
+    1. Run tests
+    2. Create or update classes to pass a single test case
+    3. Repeat
+import prompt library from README copy.md
+ask the following questions:
+    it is my understanding that you want to build an engineering team? 
+        yes or no
+    how many employees for the team (1-10)
 use prompt to ask for new employee from user
     name, id, email, employee type (string)
 add employee to array/list of employees
@@ -10,3 +16,6 @@ loop over all employees and generate htlm
 show html to person 
 
 */
+
+const inquirer = require("inquirer");
+const jest = require("jest");
