4b3ec990fbd7b737d9a7777967f98f1f9f7d64b8
Pat Brown <ptrckbrown94@gmail.com>
2019-12-12 20:40:00 -0600

    added html to professions and worked on app functions

--- app.js
+++ app.js
@@ -24,27 +24,42 @@ const inquirer = require("inquirer");
 inquirer.prompt({
     message: "It is my understanding that you want to build an engineering team?",
     name: "answer"
-    })
-.then(function({answer, }){
-console.log(answer)
-if(answer === "yes"){
+})
+    .then(function ({ answer, }) {
+        console.log(answer)
+        if (answer === "yes") {
 
-   setUpQuestions()
-} else {
+            setUpQuestions()
+        } else {
 
-console.log("I guess we're all done!")
-}
+            console.log("I guess we're all done!")
+        }
 
-});
+    });
 
-function setUpQuestions(){
-    inquirer.prompt({
+function setUpQuestions() {
+    inquirer.prompt([{
+        type: "question",
+        message: "What is the team member's name?",
+        name: "name"
+    }, 
+
+    {
+        type: "question",
+        message:"what is the employee's ID",
+        name:"id"
+
+    },
+
+
+
+    {
         type: "list",
         message: "How big do you want your team?",
-        choices: [2, 3, 4, 5, 6, 7, 8], 
+        choices: [2, 3, 4, 5, 6, 7, 8],
         name: "answer"
-        })
-        .then(function(response) {
+    }])
+        .then(function (response) {
             createEmployees(response.answer)
         })
 }
@@ -63,11 +78,55 @@ function createEmployees(numberOfEmployees) {
         // continue doing stuff
     }
 }
-function generalQuestions(){
+function generalQuestions() {
     //array of questions for inquirer 
     //.thenk(function(response))
     // switch statements 
+
+    inquirer.prompt([
+
+        {
+            type: "list",
+            message: "What title does the team member have?",
+            choices: ["Manager", "Enginer", "Intern"],
+            name: "answer"
+        })
+        .then(function (response) {
+            createEmployees(response.answer)
+        })
+
+
     // if they say enginer => engineeerQuestion 
     // if they say intern => internQuestion
     // If they say manager => managerQuestion
 }
+
+
+const newIntern = new Intern(input.name, input.id, input.email, response.school)
+// console.log(newIntern)
+// {
+// name: input.name,
+// id: input.id,
+// email: input.email
+// school: resposnse.school
+// }
+
+const newManager = new newManager(input.name, input.id, input.email, response.officeNumber)
+// console.log(newManager)
+// {
+// name: input.name,
+// id: input.id
+// email: input.email
+// office: input.officeNmber    
+// }
+
+function internQuestion(input) {
+    // inquirer stuff
+    // .then(function(response) {
+    //    console.log(response) - engineer question and answer
+    //    console.log(input) - intern question and answer
+    //    create a new Intern with the responses
+    // })
+}
+// [ q4. question specific to job]
+
